06ae294d35bb908661a7f5576141349c17645c1d
alecsp27 <alecsp27@yahoo.com>
2025-03-09 19:15:35 +0200

    Created Android Studio Project and added paho mqtt

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        maven("https://repo.eclipse.org/content/repositories/paho-snapshots/")
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://repo.eclipse.org/content/repositories/paho-snapshots/")
+    }
+}
+
+rootProject.name = "SS_Project"
+include(":app")
