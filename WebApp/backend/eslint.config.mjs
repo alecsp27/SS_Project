4934203a34d4e0a02a164b5b05fa935faf3a8d97
alexmaxim43 <alex.maxim02@yahoo.com>
2025-05-26 00:30:12 +0300

    eslint

---
+++ WebApp/backend/eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    ignores: ["dist/**", "node_modules/**"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+]);
